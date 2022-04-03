c996968b1dfff28ef550907b231a74d86fe079c4
zeggel <wickac@bk.ru>
2022-04-03 14:49:23 +0300

    Stop trown barrels on wall

--- src/ThrowableObject.lua
+++ src/ThrowableObject.lua
@@ -4,6 +4,7 @@ function ThrowableObject:init(def, x , y)
     GameObject.init(self, def, x, y)
     self.liftable = true
     self.thrown = false
+    self.bumped = false
     self.direction = 'left'
     self.speed = 80
 end
@@ -12,12 +13,39 @@ function ThrowableObject:update(dt)
     if self.thrown then
         if self.direction == 'left' then
             self.x = self.x - self.speed * dt
+
+            if self.x <= MAP_RENDER_OFFSET_X + TILE_SIZE then
+                self.x = MAP_RENDER_OFFSET_X + TILE_SIZE
+                self.bumped = true
+            end
         elseif self.direction == 'right' then
             self.x = self.x + self.speed * dt
-        elseif self.derection == 'up' then
+
+            if self.x + self.width >= VIRTUAL_WIDTH - TILE_SIZE * 2 then
+                self.x = VIRTUAL_WIDTH - TILE_SIZE * 2 - self.width
+                self.bumped = true
+            end
+        elseif self.direction == 'up' then
             self.y = self.y - self.speed * dt
+
+            if self.y <= MAP_RENDER_OFFSET_Y + TILE_SIZE - self.height / 2 then
+                self.y = MAP_RENDER_OFFSET_Y + TILE_SIZE - self.height / 2
+                self.bumped = true
+            end
         elseif self.direction == 'down' then
             self.y = self.y + self.speed * dt
+
+            local bottomEdge = VIRTUAL_HEIGHT - (VIRTUAL_HEIGHT - MAP_HEIGHT * TILE_SIZE)
+                + MAP_RENDER_OFFSET_Y - TILE_SIZE
+
+            if self.y + self.height >= bottomEdge then
+                self.y = bottomEdge - self.height
+                self.bumped = true
+            end
+        end
+
+        if self.bumped then
+            self.thrown = false
         end
     end
 end
